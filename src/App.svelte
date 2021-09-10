0a3f9203bb86dbcb6026c560ab9a37dabb9c2f16
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-09-10 19:02:13 +0700

    adding some cdn

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,16 @@
-<script>
-</script>
+<svelte:head>
+    <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/p5.min.js"></script>
+    <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/addons/p5.dom.min.js"></script>
+    <script
+        src="https://unpkg.com/ml5@latest/dist/ml5.min.js"
+        type="text/javascript"></script>
+</svelte:head>
 
 <main>
-    <div class="text-center">
-        <h1>Hello World</h1>
+    <div class="text-center mt-12">
+        <h1 class="text-3xl">Handpose with WebCam</h1>
     </div>
 </main>
 
